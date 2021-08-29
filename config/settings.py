cc93f2e8ac85498d79b504be13af82e980a0464c
amin <seyedamin963@gmail.com>
2021-08-29 18:42:49 +0430

    Add media file to our project and create cover for each book

--- config/settings.py
+++ config/settings.py
@@ -139,14 +139,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATICFILES_FINDERS = [
     
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
